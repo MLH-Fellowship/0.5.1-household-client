e1af9350b764330b30e985f22a56018d257118c6
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-06-04 23:20:08 +0100

    completed add routes

--- src/components/HouseTasks/HouseTasks.js
+++ src/components/HouseTasks/HouseTasks.js
@@ -5,55 +5,56 @@ import { Col, Card } from "antd";
 import { Link } from "react-router-dom";
 import withToast from "../WithToast";
 
-
 const client = new HTTPClient(process.env.REACT_APP_API_URL, {
-    ...getAuthHeaders(),
+  ...getAuthHeaders(),
 });
 
 class HouseTasks extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            tasks: [],
-        }
-        this.fetchTasks = this.fetchTasks.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      tasks: [],
+    };
+    this.fetchTasks = this.fetchTasks.bind(this);
+  }
+  fetchTasks() {
+    client
+      .get(`/house/${this.props.houseId}/task/all`)
+      .then((result) => {
+        console.log("tasks -->", result);
+        this.setState({ tasks: result.data.data, loading: false });
+      })
+      .catch((error) => {
+        this.props.addToast("Fetched failed to fetch tasks.", {
+          appearance: "error",
+        });
+      });
+  }
+  componentDidMount() {
+    this.fetchTasks();
+  }
+  render() {
+    if (this.state.loading) {
+      return <p>Loading...</p>;
     }
-    fetchTasks() {
-        console.log(this.props);
-        client.get(`/house/${this.props.match.params.id}/task/all`)
-            .then(result => {
-                console.log(result)
-                this.setState({ tasks: result.data.data, loading: false });
-            })
-            .catch(error => {
-                this.props.addToast("Fetched failed to fetch tasks.", {
-                    appearance: "error",
-                });
-            })
-    }
-    componentDidMount() {
-        this.fetchTasks();
-    }
-    render() {
-        if (this.state.loading) {
-            return <p>Loading...</p>
-        }
-        console.log(this.state);
-        return <>
-            {
-                this.state.tasks.map((task, taskIndex) => {
-                    return <Col sm={24} md={12} lg={6} key={taskIndex}>
-                        <Card title={task.name} key={taskIndex}>
-                            <p>{task.description}</p>
-                            <p>{task.frequency}</p>
-                            <Link to={`/task/edit/${task.task_id}`}>Edit task</Link>
-                        </Card>
-                    </Col>
-                })
-            }
-        </>
-    }
+    console.log(this.state);
+    return (
+      <>
+        {this.state.tasks.map((task, taskIndex) => {
+          return (
+            <Col sm={24} md={12} lg={6} key={taskIndex}>
+              <Card title={task.name} key={taskIndex}>
+                <p>{task.description}</p>
+                <p>{task.frequency}</p>
+                <Link to={`/task/edit/${task.task_id}`}>Edit task</Link>
+              </Card>
+            </Col>
+          );
+        })}
+      </>
+    );
+  }
 }
 
 export default withToast(HouseTasks);
