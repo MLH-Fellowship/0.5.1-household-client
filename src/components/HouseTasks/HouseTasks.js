575a61af98287e1513eda10e38d006b539423318
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-06-04 23:50:28 +0100

    complete house tasks card UI

--- src/components/HouseTasks/HouseTasks.js
+++ src/components/HouseTasks/HouseTasks.js
@@ -1,13 +1,28 @@
 import React from "react";
 import HTTPClient from "../../HTTPClient";
 import { getAuthHeaders } from "../../utils";
-import { Col, Card } from "antd";
+import { Col, Card, Skeleton, Avatar, Row, Button } from "antd";
+import {
+  SettingOutlined,
+  EditOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import withToast from "../WithToast";
+
+import "./index.css";
+
+const Meta = Card.Meta;
 
 const client = new HTTPClient(process.env.REACT_APP_API_URL, {
   ...getAuthHeaders(),
 });
+
+function randomDate(start, end) {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  ).toDateString();
+}
 
 class HouseTasks extends React.Component {
   constructor(props) {
@@ -39,20 +54,53 @@ class HouseTasks extends React.Component {
       return <p>Loading...</p>;
     }
     console.log(this.state);
+    // add fallback for no cards!!
     return (
-      <>
-        {this.state.tasks.map((task, taskIndex) => {
-          return (
-            <Col sm={24} md={12} lg={6} key={taskIndex}>
-              <Card title={task.name} key={taskIndex}>
+      <div className="house-tasks">
+        <Row gutter={[24, 16]}>
+          {this.state.tasks.map((task, taskIndex) => {
+            return (
+              <>
+                <Col sm={24} md={12} lg={6} key={taskIndex}>
+                  <Card
+                    actions={[
+                      <Link to={`/task/edit/${task.task_id}`}>
+                        <EditOutlined key="edit" />
+                      </Link>,
+                      <EllipsisOutlined key="ellipsis" />,
+                    ]}
+                  >
+                    <Meta
+                      avatar={
+                        <Avatar
+                          shape="square"
+                          src="https://image.flaticon.com/icons/svg/167/167824.svg"
+                        />
+                      }
+                      title={task.name}
+                      description={task.description}
+                    />
+                    <h3 className="due-date">
+                      Due Next By -{" "}
+                      {randomDate(new Date(2020, 0, 1), new Date(2020, 5, 5))}
+                    </h3>
+                    <h3 className='frequency'>Frequency - {task.frequency}</h3>
+                    <div className="button-wrapper">
+                      <Button danger>Send Reminder</Button>
+                      <Button danger> Re assign </Button>
+                    </div>
+                  </Card>
+                  {/* <Card title={task.name} key={taskIndex}>
                 <p>{task.description}</p>
                 <p>{task.frequency}</p>
                 <Link to={`/task/edit/${task.task_id}`}>Edit task</Link>
-              </Card>
-            </Col>
-          );
-        })}
-      </>
+              </Card> */}
+                </Col>
+              </>
+            );
+          })}
+        </Row>
+      </div>
     );
   }
 }
